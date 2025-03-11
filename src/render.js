9711d68893483e1e128b5384db0324a025880fd1
sohamdasx <sohamdas.2702@gmail.com>
2025-03-11 20:59:27 +0530

    lowkey confused

--- src/render.js
+++ src/render.js
@@ -1,14 +1,45 @@
-const videoElement = document.getElementById('video');
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
-const videoSelectBtn = document.getElementById('videoSelectBtn');
+const videoElement = document.getElementById("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
 
 videoSelectBtn.onclick = getVideoSources;
 
-const { desktopCapturer } = require('electron');
+const { desktopCapturer } = require("electron");
+const { Menu } = remote;
 
-async function getVideoSources(){
-    const inputSources = await desktopCapturer.getSources({
-        types: ['window', 'screen']
-    });
+async function getVideoSources() {
+  const inputSources = await desktopCapturer.getSources({
+    types: ["window", "screen"],
+  });
+
+  const videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map((source) => {
+      return {
+        label: source.name,
+        click: () => selectSource(source),
+      };
+    })
+  );
+
+  videoOptionsMenu.popup();
+}
+
+async function selectSource(source) {
+  videoSelectBtn.innerText = source.name;
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  videoElement.srcObject = stream;
+  videoElement.play();
 }
